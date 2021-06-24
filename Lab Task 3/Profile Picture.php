a87eaf5c2fe2c4000112089efe0e736431ddcd9f
Rakir25 <84837471+Rakir25@users.noreply.github.com>
2021-06-24 15:33:42 +0600

    Add files via upload

---
+++ Lab Task 3/Profile Picture.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PROFILE PICTURE</title>
+</head>
+<body>
+  
+<form action="Upload Picture.php" method="post" enctype="multipart/form-data">
+<fieldset>
+  <legend> Select image to upload : </legend>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxwPxaw7RrT7xbGF-B_8DWFB5WOEBmmafY3TRr7fGBRngbS_JObd8pSs0GkCpOiGUfT2Q&usqp=CAU" alt="picture"><br><br>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br><br>
+  <hr><br>
+  <input type="submit" value="submit" name="submit">
+</form>
+</fieldset>
+ 
+</body>
+</html>
